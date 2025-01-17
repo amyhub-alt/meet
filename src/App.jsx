b2b9f81a412c143e0ce593501636b1e54353df83
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-01-17 08:52:39 -0500

    testing city search, event list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import EventList from './components/EventList';
+import CitySearch from './components/CitySearch'
 
 const App = () => {
- return (
-   <div className="App">
-     <div id="event-list"></div>
-   </div>
- );
+  return (
+    <div className="App">
+      <CitySearch />
+      <EventList />
+    </div>
+  );
 }
 
 
