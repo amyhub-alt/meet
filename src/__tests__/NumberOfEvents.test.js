833ac0735a7dcf101a2210a86c49ecc1ec74ac88
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-02-10 19:00:22 -0500

    Fix: Pending Test fix with NumberOfEvent

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,14 +5,13 @@ import userEvent from '@testing-library/user-event';
 
 
 test('renders a textbox element in NumberOfEvents', () => {
-    render(<NumberOfEvents />);
-     const textbox = screen.getByRole('textbox');
-
-     expect(textbox.value).toBe('32');
+    render(<NumberOfEvents setCurrentNOE={()=>{}} />);
+    const textbox = screen.getByRole('textbox');
+    expect (textbox.value).toBe('32');
 });
 
 test('updates the value of the textbox when user types in it', async () =>{
-    render(<NumberOfEvents />);
+    render(<NumberOfEvents setCurrentNOE={()=>{}} />);
     const textbox = screen.getByRole('textbox');
     await userEvent.type(textbox, '{backspace}{backspace}10');
     expect (textbox.value).toBe('10');
