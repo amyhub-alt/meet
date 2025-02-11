61ad22e567847d0dc88883db5f47c60c3b6359fa
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-02-10 21:18:35 -0500

    update NumberOfEvents.jsx

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -4,10 +4,16 @@ import NumberOfEvents from '../components/NumberOfEvents';
 import userEvent from '@testing-library/user-event';
 
 
-test('renders a textbox element in NumberOfEvents', () => {
+// test('renders a textbox element in NumberOfEvents', () => {
+//     render(<NumberOfEvents setCurrentNOE={()=>{}} />);
+//     const textbox = screen.getByRole('textbox');
+//     expect (textbox.value).toBe('32');
+// });
+
+test('check that NumberOfEvent exist', async () =>{
     render(<NumberOfEvents setCurrentNOE={()=>{}} />);
     const textbox = screen.getByRole('textbox');
-    expect (textbox.value).toBe('32');
+    expect(textbox.value).toBe("");
 });
 
 test('updates the value of the textbox when user types in it', async () =>{
