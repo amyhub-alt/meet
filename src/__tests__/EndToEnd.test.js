c1b794837b527cd4578db8f103716c103f4606f7
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-02-20 20:25:18 -0500

    fixed hide and show in endtoend.test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -25,15 +25,15 @@ describe('show/hide an event details', ()=>{
     });
 
     test('User can expand an event to see its details', async () => {
-        await page.click('.event');
-        const eventDetails = await page.$('.event');
-        expect(eventDetails).toBeDefined();
+        await page.click('.event .details');
+        const eventDescription = await page.$('.event-description');
+        expect(eventDescription).toBeDefined();
       });
     
     test('User can collapse an event to hide details', async () => {
-        await page.click('.event');
-        const eventDetails = await page.$('.event');
-        expect(eventDetails).toBeNull();
+        await page.click('.event .details');
+        const eventDescription = await page.$('.event-description');
+        expect(eventDescription).toBeNull();
     });
 
 });
