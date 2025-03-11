e191a8278c2610342b7421a06c073b06e11fbe19
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-03-11 17:04:04 -0400

    outer radius-150

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -47,14 +47,14 @@ const EventGenresChart = ({events}) => {
 
     return (
         <ResponsiveContainer width="99%" height={400}>
-          <PieChart margin={{ left: 50, right: 50 }}>
+          <PieChart>
             <Pie
               data={data}
               dataKey="value"
               fill="#8884d8"
               labelLine={false}
               label= {renderCustomizedLabel}
-              outerRadius={130}           
+              outerRadius={150}           
             >
             {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
