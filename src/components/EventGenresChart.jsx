a0d956bc38a13e449b7cb19f0f7ee62aba4a3b9a
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-03-11 17:05:29 -0400

    test

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -54,7 +54,7 @@ const EventGenresChart = ({events}) => {
               fill="#8884d8"
               labelLine={false}
               label= {renderCustomizedLabel}
-              outerRadius={150}           
+              outerRadius={110}           
             >
             {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
