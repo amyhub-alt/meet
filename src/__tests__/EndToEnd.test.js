fc3158601fa13dceb08c7bf2c71b87ccedebd06d
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-03-12 13:49:57 -0400

    fixed testing

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import puppeteer from "puppeteer";
 
 describe('show/hide an event details', ()=>{
--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
